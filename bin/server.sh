fd39a8d67069792d7e57ca083c17fedf2ef78782
Alexander Verbruggen <alex@nabu.be>
2017-05-24 10:17:09 +0200

    Added monocle settings

--- bin/server.sh
+++ bin/server.sh
@@ -1,1 +1,1 @@
-java -Ddevelopment=false -Dversion=2 -cp "lib/*:." be.nabu.eai.server.Standalone "$@"
+java -Dglass.platform=Monocle -Dmonocle.platform=Headless -Ddevelopment=false -Dversion=2 -cp "lib/*:." be.nabu.eai.server.Standalone "$@"
